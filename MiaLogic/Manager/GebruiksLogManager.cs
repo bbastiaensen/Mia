1e1060db0220e462b5c908d93c664ca32652fc0f
Ben Bastiaensen <ben.bastiaensen@talentenschoolturnhout.be>
2023-10-12 15:25:54 +0200

    Initiële code

---
+++ MiaLogic/Manager/GebruiksLogManager.cs
@@ -0,0 +1,44 @@
+﻿using MiaLogic.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiaLogic.Manager
+{
+    public static class GebruiksLogManager
+    {
+        public static string ConnectionString { get; set; }
+
+        public static List<GebruiksLog> GetGebruiksLogs()
+        {
+            List<GebruiksLog> gebruiksLogs = null;
+
+            throw new NotImplementedException();
+
+            return gebruiksLogs;
+        }
+
+        public static GebruiksLog GetGebruiksLog(int gebruiksLogId)
+        {
+            GebruiksLog gebruiksLog = null;
+
+            throw new NotImplementedException();
+
+            return gebruiksLog;
+        }
+
+        public static void SaveGebruiksLog(GebruiksLog gebruiksLog, bool insert)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DeleteGebruiksLog(GebruiksLog gebruiksLog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
