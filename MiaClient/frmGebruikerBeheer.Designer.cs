e761a4238525d40ae3ef3d29c5759420bf342112
lordoliver738 <pottersolivier99@gmail.com>
2023-12-04 15:19:54 +0100

    ik heb de filters van de lijst gemaakt.

--- MiaClient/frmGebruikerBeheer.Designer.cs
+++ MiaClient/frmGebruikerBeheer.Designer.cs
@@ -35,33 +35,42 @@
             this.RadAlle = new System.Windows.Forms.RadioButton();
             this.RadNonActief = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtGebruikersnaam = new System.Windows.Forms.TextBox();
+            this.DdlActief = new System.Windows.Forms.ComboBox();
+            this.BtnOpslaan = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // LstGebruikers
             // 
             this.LstGebruikers.FormattingEnabled = true;
-            this.LstGebruikers.ItemHeight = 16;
-            this.LstGebruikers.Location = new System.Drawing.Point(12, 12);
+            this.LstGebruikers.ItemHeight = 28;
+            this.LstGebruikers.Location = new System.Drawing.Point(16, 21);
+            this.LstGebruikers.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.LstGebruikers.Name = "LstGebruikers";
-            this.LstGebruikers.Size = new System.Drawing.Size(507, 356);
+            this.LstGebruikers.Size = new System.Drawing.Size(424, 172);
             this.LstGebruikers.TabIndex = 0;
             this.LstGebruikers.SelectedIndexChanged += new System.EventHandler(this.LstGebruikers_SelectedIndexChanged);
             // 
             // BtnPasaan
             // 
-            this.BtnPasaan.Location = new System.Drawing.Point(198, 374);
+            this.BtnPasaan.Location = new System.Drawing.Point(232, 455);
+            this.BtnPasaan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BtnPasaan.Name = "BtnPasaan";
-            this.BtnPasaan.Size = new System.Drawing.Size(180, 43);
+            this.BtnPasaan.Size = new System.Drawing.Size(208, 57);
             this.BtnPasaan.TabIndex = 1;
             this.BtnPasaan.Text = "Aanpassen";
             this.BtnPasaan.UseVisualStyleBackColor = true;
+            this.BtnPasaan.Click += new System.EventHandler(this.BtnPasaan_Click);
             // 
             // BtnVerwijder
             // 
-            this.BtnVerwijder.Location = new System.Drawing.Point(12, 374);
+            this.BtnVerwijder.Location = new System.Drawing.Point(16, 455);
+            this.BtnVerwijder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BtnVerwijder.Name = "BtnVerwijder";
-            this.BtnVerwijder.Size = new System.Drawing.Size(180, 43);
+            this.BtnVerwijder.Size = new System.Drawing.Size(208, 57);
             this.BtnVerwijder.TabIndex = 2;
             this.BtnVerwijder.Text = "Verwijderen";
             this.BtnVerwijder.UseVisualStyleBackColor = true;
@@ -69,34 +78,40 @@
             // RadActief
             // 
             this.RadActief.AutoSize = true;
-            this.RadActief.Location = new System.Drawing.Point(21, 21);
+            this.RadActief.Location = new System.Drawing.Point(29, 37);
+            this.RadActief.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.RadActief.Name = "RadActief";
             this.RadActief.Size = new System.Drawing.Size(61, 20);
             this.RadActief.TabIndex = 3;
             this.RadActief.Text = "Actief";
             this.RadActief.UseVisualStyleBackColor = true;
+            this.RadActief.CheckedChanged += new System.EventHandler(this.RadActief_CheckedChanged);
             // 
             // RadAlle
             // 
             this.RadAlle.AutoSize = true;
             this.RadAlle.Checked = true;
-            this.RadAlle.Location = new System.Drawing.Point(21, 73);
+            this.RadAlle.Location = new System.Drawing.Point(29, 128);
+            this.RadAlle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.RadAlle.Name = "RadAlle";
             this.RadAlle.Size = new System.Drawing.Size(51, 20);
             this.RadAlle.TabIndex = 4;
             this.RadAlle.TabStop = true;
             this.RadAlle.Text = "Alle";
             this.RadAlle.UseVisualStyleBackColor = true;
+            this.RadAlle.CheckedChanged += new System.EventHandler(this.RadAlle_CheckedChanged);
             // 
             // RadNonActief
             // 
             this.RadNonActief.AutoSize = true;
-            this.RadNonActief.Location = new System.Drawing.Point(21, 47);
+            this.RadNonActief.Location = new System.Drawing.Point(29, 82);
+            this.RadNonActief.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.RadNonActief.Name = "RadNonActief";
             this.RadNonActief.Size = new System.Drawing.Size(88, 20);
             this.RadNonActief.TabIndex = 5;
             this.RadNonActief.Text = "Niet Actief";
             this.RadNonActief.UseVisualStyleBackColor = true;
+            this.RadNonActief.CheckedChanged += new System.EventHandler(this.RadNonActief_CheckedChanged);
             // 
             // groupBox1
             // 
@@ -104,28 +119,86 @@
             this.groupBox1.Controls.Add(this.RadAlle);
             this.groupBox1.Controls.Add(this.RadNonActief);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
-            this.groupBox1.Location = new System.Drawing.Point(525, 12);
+            this.groupBox1.Location = new System.Drawing.Point(448, 21);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(142, 110);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(195, 172);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filter";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 28);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Gebruikersnaam: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 28);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Actief: ";
+            // 
+            // TxtGebruikersnaam
+            // 
+            this.TxtGebruikersnaam.Location = new System.Drawing.Point(17, 276);
+            this.TxtGebruikersnaam.Name = "TxtGebruikersnaam";
+            this.TxtGebruikersnaam.Size = new System.Drawing.Size(423, 34);
+            this.TxtGebruikersnaam.TabIndex = 12;
+            // 
+            // DdlActief
+            // 
+            this.DdlActief.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DdlActief.FormattingEnabled = true;
+            this.DdlActief.Items.AddRange(new object[] {
+            "Ja",
+            "Nee"});
+            this.DdlActief.Location = new System.Drawing.Point(17, 345);
+            this.DdlActief.Name = "DdlActief";
+            this.DdlActief.Size = new System.Drawing.Size(121, 36);
+            this.DdlActief.TabIndex = 13;
+            // 
+            // BtnOpslaan
+            // 
+            this.BtnOpslaan.Location = new System.Drawing.Point(447, 455);
+            this.BtnOpslaan.Name = "BtnOpslaan";
+            this.BtnOpslaan.Size = new System.Drawing.Size(208, 57);
+            this.BtnOpslaan.TabIndex = 14;
+            this.BtnOpslaan.Text = "Opslaan";
+            this.BtnOpslaan.UseVisualStyleBackColor = true;
+            this.BtnOpslaan.Visible = false;
             // 
             // frmGebruikerBeheer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(935, 530);
+            this.Controls.Add(this.BtnOpslaan);
+            this.Controls.Add(this.DdlActief);
+            this.Controls.Add(this.TxtGebruikersnaam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.BtnVerwijder);
             this.Controls.Add(this.BtnPasaan);
             this.Controls.Add(this.LstGebruikers);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmGebruikerBeheer";
             this.Text = "frmGebruikerBeheer";
             this.Load += new System.EventHandler(this.frmGebruikerBeheer_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -138,5 +211,10 @@
         private System.Windows.Forms.RadioButton RadAlle;
         private System.Windows.Forms.RadioButton RadNonActief;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtGebruikersnaam;
+        private System.Windows.Forms.ComboBox DdlActief;
+        private System.Windows.Forms.Button BtnOpslaan;
     }
 }
--- MiaClient/frmGebruikerBeheer.cs
+++ MiaClient/frmGebruikerBeheer.cs
@@ -8,11 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MiaLogic.Manager;
+using MiaLogic.Object;
 
 namespace MiaClient
 {
     public partial class frmGebruikerBeheer : Form
     {
+        List<Gebruiker> gebruikers;
+
+
         public frmGebruikerBeheer()
         {
             InitializeComponent();
@@ -20,17 +24,62 @@ namespace MiaClient
 
         private void LstGebruikers_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void frmGebruikerBeheer_Load(object sender, EventArgs e)
         {
+            gebruikers = GebruikerManager.GetGebruikers();
+            
+            LstGebruikers.DisplayMember = "Gebruikersnaam";
+            LstGebruikers.ValueMember = "Id";
+            LstGebruikers.DataSource = gebruikers;
+           
+
+
+
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadNonActief_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadNonActief.Checked == true)
+            {
+                LstGebruikers.DataSource = gebruikers.Where(g => g.Actief == false).ToList();
+            }
+        }
+
+        private void RadActief_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadActief.Checked == true)
+            {
+                LstGebruikers.DataSource = gebruikers.Where(g => g.Actief == true).ToList();
+            }
+        }
+
+        private void RadAlle_CheckedChanged(object sender, EventArgs e)
+        {
             if (RadAlle.Checked == true)
             {
-                
-                LstGebruikers.Items.Add(GebruikerManager.GetGebruikers());
+                LstGebruikers.DataSource = gebruikers;
             }
-            
+        }
+
+        private void BtnPasaan_Click(object sender, EventArgs e)
+        {
+            BtnOpslaan.Visible = true;
+            TxtGebruikersnaam.Text = LstGebruikers.SelectedIndex.ToString();
+           Gebruiker gebruiker1 = GebruikerManager.GetGebruikerByGebruikersnaam(TxtGebruikersnaam.Text);
+           bool actief = gebruiker1.Actief;
+            if (actief == true)
+            {
+                
+            }
         }
     }
 }
