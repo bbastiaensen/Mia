96ac88e481909ad7cab5b2dd3b64ae138b7b2069
Charlotte Sterckx <csterckx@outlook.com>
2024-02-19 15:15:45 +0100

    User Story 1 - Taak 1 - AFGEROND

--- MiaClient/frmAanvraagFormulier.cs
+++ MiaClient/frmAanvraagFormulier.cs
@@ -1,5 +1,5 @@
 ﻿using MiaLogic.Object;
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,12 +19,116 @@ namespace MiaClient
             vulFormulier();
         }
 
+
+        // Vullen van de dropdownlists
+        public void VulAanvraagId()
+        {
+            int highestAanvraagId = MiaLogic.Manager.AanvraagManager.GetHighestAanvraagId();
+
+            txtAanvraagId.Text = (highestAanvraagId + 1).ToString();
+        }
+        public void VulAfdelingDropDown(ComboBox cmbAfdeling)
+        {
             List<Afdeling> afdelingen = MiaLogic.Manager.AanvraagManager.GetAfdelingen();
+
+            cmbAfdeling.DataSource = afdelingen;
+            cmbAfdeling.DisplayMember = "Naam";
+            cmbAfdeling.ValueMember = "Id";
+            cmbAfdeling.SelectedIndex = -1;
+        }
+        public void VulDienstDropDown(ComboBox cmbDienst)
+        {
+            List<Dienst> diensten = MiaLogic.Manager.AanvraagManager.GetDiensten();
+
+            cmbDienst.DataSource = diensten;
+            cmbDienst.DisplayMember = "Naam";
+            cmbDienst.ValueMember = "Id";
+            cmbDienst.SelectedIndex = -1;
+        }
+        public void VulPrioriteitDropDown(ComboBox cmbPrioriteit)
+        {
+            List<Prioriteit> prioriteiten = MiaLogic.Manager.AanvraagManager.GetPrioriteiten();
+
+            cmbPrioriteit.DataSource = prioriteiten;
+            cmbPrioriteit.DisplayMember = "Naam";
+            cmbPrioriteit.ValueMember = "Id";
+            cmbPrioriteit.SelectedIndex = -1;
+        }
+        public void VulFinancieringDropDown(ComboBox cmbFinanciering)
+        {
+            List<Financiering> financieringen = MiaLogic.Manager.AanvraagManager.GetFinancieringen();
+
+            cmbFinanciering.DataSource = financieringen;
+            cmbFinanciering.DisplayMember = "Naam";
+            cmbFinanciering.ValueMember = "Id";
+            cmbFinanciering.SelectedIndex = -1;
+        }
+        public void VulInvesteringDropDown(ComboBox cmbInvestering)
+        {
+            List<Investering> investeringen = MiaLogic.Manager.AanvraagManager.GetInvesteringen();
+
+            cmbInvestering.DataSource = investeringen;
+            cmbInvestering.DisplayMember = "Naam";
+            cmbInvestering.ValueMember = "Id";
+            cmbInvestering.SelectedIndex = -1;
+        }
+        public void VulFinancieringsjaarDropDown(ComboBox cmbFinancieringsjaar)
+        {
+            List<string> financieringsjaren = MiaLogic.Manager.AanvraagManager.GetFinancieringsjaren();
+
+            cmbFinancieringsjaar.DataSource = financieringsjaren;
+            cmbFinancieringsjaar.SelectedIndex = -1;
+        }
+        public void VulKostenplaatsDropDown(ComboBox cmbKostenplaats)
+        {
+            List<Kostenplaats> kostenplaatsen = MiaLogic.Manager.AanvraagManager.GetKostenplaatsen();
+
+            cmbKostenplaats.DataSource = kostenplaatsen;
+            cmbKostenplaats.DisplayMember = "Naam";
+            cmbKostenplaats.ValueMember = "Id";
+            cmbKostenplaats.SelectedIndex = -1;
+        }
+        public void VulAankoperDropDown(ComboBox cmbAankoper)
+        {
+            List<string> aankoper = MiaLogic.Manager.AanvraagManager.GetWieKooptHet();
+
+            cmbAankoper.DataSource = aankoper;
+            cmbAankoper.SelectedIndex = -1;
+        }
+        private decimal BerekenTotaalprijs()
+        {
+            // Controleer of tekstvak prijsindicatie niet leeg is en de waarde is numeriek.
+            if (string.IsNullOrEmpty(txtPrijsindicatie.Text) || !decimal.TryParse(txtPrijsindicatie.Text, out decimal prijsIndicatie))
+            {
+                return 0;
+            }
+
+            // Controleer of tekstvak aantalstuks niet leeg is en de waarde is numeriek.
+
+            if (string.IsNullOrEmpty(txtAantalStuks.Text) || !int.TryParse(txtAantalStuks.Text, out int aantalStuks))
+            {
+                return 0;
+            }
+
+            // Als beide een correcte waarde hebben berekenen we de totaalprijs
+            decimal totaalprijs = prijsIndicatie * aantalStuks;
+            return totaalprijs;
+        }
         public void vulFormulier()
         {
             txtGebruiker.Text = Program.Gebruiker;
-            txtAanvraagmoment.Text = System.DateTime.Now.Date.ToString();
+            txtAanvraagmoment.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            VulAanvraagId();
+            // Identificatie
             VulAfdelingDropDown(ddlAfdeling);
+            VulDienstDropDown(ddlDienst);
+            // Investering
+            VulPrioriteitDropDown(ddlPrioriteit);
+            VulFinancieringDropDown(ddlFinanciering);
+            VulInvesteringDropDown(ddlInvestering);
+            VulFinancieringsjaarDropDown(ddlFinancieringsjaar);
+            VulKostenplaatsDropDown(ddlKostenplaats);
+            VulAankoperDropDown(ddlWieKooptHet);
         }
 
 
--- MiaLogic/Manager/AanvraagManager.cs
+++ MiaLogic/Manager/AanvraagManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,7 +36,7 @@ namespace MiaLogic.Manager
 
                     while (objRea.Read())
                     {
-                        if(returnlist == null)
+                        if (returnlist == null)
                         {
                             returnlist = new List<Aanvraag>();
                         }
@@ -51,12 +52,260 @@ namespace MiaLogic.Manager
                         a.PrijsIndicatieStuk = Convert.ToDecimal(objRea["PrijsIndicatieStuk"]);
                         a.Kostenplaats = objRea["Kostenplaats"].ToString();
 
-                        
+
                         returnlist.Add(a);
                     }
                 }
-            }           
+            }
             return returnlist;
+        }
+
+        public static int GetHighestAanvraagId()
+        {
+            int highestAanvraagId = 0;
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "SELECT MAX(Id) AS highest FROM Aanvraag;";
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                                highestAanvraagId = Convert.ToInt32(reader["highest"]);
+                        }
+                    }
+                }
+            }
+
+            return highestAanvraagId;
+        }
+
+
+        // Data uit databank halen
+        public static List<Afdeling> GetAfdelingen()
+        {
+            List<Afdeling> afdelingen = new List<Afdeling>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Id, Naam FROM Afdeling ORDER BY Naam ASC";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Afdeling afdeling = new Afdeling
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Naam = reader["Naam"].ToString()
+                            };
+
+                            afdelingen.Add(afdeling);
+                        }
+                    }
+                }
+            }
+
+            return afdelingen;
+        }
+        public static List<Dienst> GetDiensten()
+        {
+            List<Dienst> diensten = new List<Dienst>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Id, Naam FROM Dienst ORDER BY Naam ASC";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Dienst dienst = new Dienst
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Naam = reader["Naam"].ToString()
+                            };
+
+                            diensten.Add(dienst);
+                        }
+                    }
+                }
+            }
+
+            return diensten;
+        }
+        public static List<Prioriteit> GetPrioriteiten()
+        {
+            List<Prioriteit> prioriteiten = new List<Prioriteit>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Id, Naam FROM Prioriteit ORDER BY Naam ASC";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Prioriteit prioriteit = new Prioriteit
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Naam = reader["Naam"].ToString()
+                            };
+
+                            prioriteiten.Add(prioriteit);
+                        }
+                    }
+                }
+            }
+
+            return prioriteiten;
+        }
+        public static List<Financiering> GetFinancieringen()
+        {
+            List<Financiering> financieringen = new List<Financiering>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Id, Naam FROM FinancieringsType ORDER BY Naam ASC";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Financiering financiering = new Financiering
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Naam = reader["Naam"].ToString()
+                            };
+
+                            financieringen.Add(financiering);
+                        }
+                    }
+                }
+            }
+
+            return financieringen;
+        }
+        public static List<Investering> GetInvesteringen()
+        {
+            List<Investering> investeringen = new List<Investering>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Id, Naam FROM InvesteringsType ORDER BY Naam ASC";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Investering investering = new Investering
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Naam = reader["Naam"].ToString()
+                            };
+
+                            investeringen.Add(investering);
+                        }
+                    }
+                }
+            }
+
+            return investeringen;
+        }
+        public static List<string> GetFinancieringsjaren()
+        {
+            List<string> financieringsjaren = new List<string>();
+
+            // Adding financial years based on the current year
+            int currentYear = DateTime.Now.Year;
+            for (int i = 0; i < 5; i++)
+            {
+                string startingYear = (currentYear + i).ToString();
+                financieringsjaren.Add(startingYear);
+            }
+
+            return financieringsjaren;
+        }
+        public static List<Kostenplaats> GetKostenplaatsen()
+        {
+            List<Kostenplaats> kostenplaatsen = new List<Kostenplaats>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Id, Naam FROM Kostenplaats ORDER BY Naam ASC";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Kostenplaats kostenplaats = new Kostenplaats
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Naam = reader["Naam"].ToString()
+                            };
+
+                            kostenplaatsen.Add(kostenplaats);
+                        }
+                    }
+                }
+            }
+
+            return kostenplaatsen;
+        }
+        public static List<string> GetWieKooptHet()
+        {
+            List<string> aankoper = new List<string>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Voornaam + ' ' + Achternaam AS FullName FROM Aankoper ORDER BY FullName ASC";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string fullName = reader["FullName"].ToString();
+                            aankoper.Add(fullName);
+                        }
+                    }
+                }
+            }
+
+            return aankoper;
         }
     }
 }
