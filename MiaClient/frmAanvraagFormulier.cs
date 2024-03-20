e32ec2052e44595ff70bd11e8a064f56db2682f3
Timzz <bigtimiscool@gmail.com>
2024-03-20 12:28:12 +0100

    Hele hoop nieuwe changes

--- MiaClient/frmAanvraagFormulier.cs
+++ MiaClient/frmAanvraagFormulier.cs
@@ -10,6 +10,7 @@ using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
