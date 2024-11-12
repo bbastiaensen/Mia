1b960274e50ceef43513df37962f0a46c0ceea39
Quinten Taelman <quinten.taelman@fluxus.nu>
2024-11-12 13:36:55 +0100

    Vinden wat er moet gebeuren en begin met het maken van het filter systeem

--- MiaClient/frmGoedkeuring.Designer.cs
+++ MiaClient/frmGoedkeuring.Designer.cs
@@ -34,20 +34,20 @@
             // pnlGoedkeuringen
             // 
             this.pnlGoedkeuringen.AutoScroll = true;
-            this.pnlGoedkeuringen.Location = new System.Drawing.Point(9, 11);
-            this.pnlGoedkeuringen.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlGoedkeuringen.Location = new System.Drawing.Point(12, 238);
+            this.pnlGoedkeuringen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlGoedkeuringen.Name = "pnlGoedkeuringen";
-            this.pnlGoedkeuringen.Size = new System.Drawing.Size(1036, 426);
+            this.pnlGoedkeuringen.Size = new System.Drawing.Size(1381, 300);
             this.pnlGoedkeuringen.TabIndex = 0;
             // 
             // frmGoedkeuring
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1052, 446);
+            this.ClientSize = new System.Drawing.Size(1403, 549);
             this.Controls.Add(this.pnlGoedkeuringen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "frmGoedkeuring";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
