0fcdcf310d97542074237ba02b054b9be60fdad9
rune van hofstraeten <runevanhofstraeten@hotmail.com>
2025-01-13 13:26:42 +0100

    budgetspreiding formulier

--- MiaClient/frmBudgetspreiding.Designer.cs
+++ MiaClient/frmBudgetspreiding.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MiaClient
 {
-    partial class frmBudgetspreiding
+    partial class frmBudgetSpreiding
     {
         /// <summary>
         /// Required designer variable.
@@ -28,84 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlBudgetspreiding = new System.Windows.Forms.Panel();
-            this.gbxFinanciersjaar = new System.Windows.Forms.GroupBox();
-            this.lblFinancieringsjaar = new System.Windows.Forms.Label();
-            this.btnExport = new System.Windows.Forms.Button();
-            this.cmbJaar = new System.Windows.Forms.ComboBox();
-            this.gbxFinanciersjaar.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pnlBudgetspreiding
+            // frmBudgetSpreiding
             // 
-            this.pnlBudgetspreiding.Location = new System.Drawing.Point(12, 167);
-            this.pnlBudgetspreiding.Name = "pnlBudgetspreiding";
-            this.pnlBudgetspreiding.Size = new System.Drawing.Size(776, 271);
-            this.pnlBudgetspreiding.TabIndex = 0;
-            // 
-            // gbxFinanciersjaar
-            // 
-            this.gbxFinanciersjaar.Controls.Add(this.cmbJaar);
-            this.gbxFinanciersjaar.Controls.Add(this.btnExport);
-            this.gbxFinanciersjaar.Controls.Add(this.lblFinancieringsjaar);
-            this.gbxFinanciersjaar.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gbxFinanciersjaar.Location = new System.Drawing.Point(49, 41);
-            this.gbxFinanciersjaar.Name = "gbxFinanciersjaar";
-            this.gbxFinanciersjaar.Size = new System.Drawing.Size(652, 100);
-            this.gbxFinanciersjaar.TabIndex = 1;
-            this.gbxFinanciersjaar.TabStop = false;
-            this.gbxFinanciersjaar.Text = "Selecteer de richtperiode";
-            // 
-            // lblFinancieringsjaar
-            // 
-            this.lblFinancieringsjaar.AutoSize = true;
-            this.lblFinancieringsjaar.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFinancieringsjaar.Location = new System.Drawing.Point(32, 48);
-            this.lblFinancieringsjaar.Name = "lblFinancieringsjaar";
-            this.lblFinancieringsjaar.Size = new System.Drawing.Size(151, 25);
-            this.lblFinancieringsjaar.TabIndex = 0;
-            this.lblFinancieringsjaar.Text = "Financieringsjaar :";
-            // 
-            // btnExport
-            // 
-            this.btnExport.Location = new System.Drawing.Point(488, 38);
-            this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(111, 35);
-            this.btnExport.TabIndex = 1;
-            this.btnExport.Text = "Exporteer";
-            this.btnExport.UseVisualStyleBackColor = true;
-            // 
-            // cmbJaar
-            // 
-            this.cmbJaar.FormattingEnabled = true;
-            this.cmbJaar.Location = new System.Drawing.Point(256, 45);
-            this.cmbJaar.Name = "cmbJaar";
-            this.cmbJaar.Size = new System.Drawing.Size(121, 33);
-            this.cmbJaar.TabIndex = 2;
-            // 
-            // frmBudgetspreiding
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.gbxFinanciersjaar);
-            this.Controls.Add(this.pnlBudgetspreiding);
-            this.MaximizeBox = false;
-            this.Name = "frmBudgetspreiding";
-            this.Text = "frmBudgetspreiding";
-            this.Load += new System.EventHandler(this.frmBudgetspreiding_Load);
-            this.gbxFinanciersjaar.ResumeLayout(false);
-            this.gbxFinanciersjaar.PerformLayout();
+            this.ClientSize = new System.Drawing.Size(1200, 726);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "frmBudgetSpreiding";
+            this.Text = "Budgetoverzicht";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBudgetSpreiding_FormClosing);
+            this.Load += new System.EventHandler(this.frmBudgetSpreiding_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel pnlBudgetspreiding;
-        private System.Windows.Forms.GroupBox gbxFinanciersjaar;
-        private System.Windows.Forms.Label lblFinancieringsjaar;
-        private System.Windows.Forms.Button btnExport;
-        private System.Windows.Forms.ComboBox cmbJaar;
     }
 }
--- MiaClient/frmBudgetspreiding.cs
+++ MiaClient/frmBudgetspreiding.cs
@@ -1,39 +1,49 @@
-﻿using MiaLogic.Manager;
+﻿using ProofOfConceptDesign;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MiaClient
 {
-    public partial class frmBudgetspreiding : Form
+    public partial class frmBudgetSpreiding : Form
     {
-        public frmBudgetspreiding()
+        public frmBudgetSpreiding()
         {
             InitializeComponent();
         }
 
-        private void frmBudgetspreiding_Load(object sender, EventArgs e)
+        private void frmBudgetSpreiding_FormClosing(object sender, FormClosingEventArgs e)
         {
-           
-             
-                
-
-
-            List<string> financieringsjaren = FinancieringsjaarManager.GetFinancieringsjaren();
-            foreach (string jaren in financieringsjaren) 
-            {
-                cmbJaar.Items.Add(jaren);
-            }
-            
-
+            //We sluiten het formulier niet, maar verbergen het. Zo voorkomen we dat het formulier meerdere
+            //keren naast elkaar kan geopend worden.
+            e.Cancel = true;
+            ((Form)sender).Hide();
         }
-        
+
+        private void frmBudgetSpreiding_Load(object sender, EventArgs e)
+        {
+            CreateUI();
+        }
+
+        private void CreateUI()
+        {
+            //Achtergrondkleur instellen op parameterwaarde
+            this.BackColor = StyleParameters.Achtergrondkleur;
+
+            //Opmaak buttons instellen op parameterwaardes
+            foreach (var btn in this.Controls.OfType<Button>())
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 0;
+                btn.BackColor = StyleParameters.ButtonBack;
+                btn.ForeColor = StyleParameters.Buttontext;
+            }
+        }
     }
 }
