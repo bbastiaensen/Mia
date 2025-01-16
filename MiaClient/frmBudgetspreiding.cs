eb5d4505ab956699c10f6f33d12f44fa32464f21
lise vermeiren <lisevermeiren.meloen@gmail.com>
2025-01-16 09:20:30 +0100

    eh

--- MiaClient/frmBudgetspreiding.cs
+++ MiaClient/frmBudgetspreiding.cs
@@ -1,4 +1,5 @@
 ﻿using MiaLogic.Manager;
+using MiaLogic.Object;
 using ProofOfConceptDesign;
 using System;
 using System.Collections.Generic;
@@ -64,93 +65,114 @@ namespace MiaClient
             object Nothing = System.Reflection.Missing.Value;
             Excel.Workbook workBook = app.Workbooks.Add(Nothing);
             Excel.Worksheet worksheet = (Excel.Worksheet)workBook.Sheets[1];
-            // Write data+layout
-            int m = 0;
-            for (int i = 0; m < 12; i++)
-            {
-                string r1 = "A" + (i + 2);
-                string r2 = "B" + (i + 2);
-                string r3 = "C" + (i + 2);
-                if (m % 2 == 0 && m != 0)
-                {
-                    worksheet.get_Range(r1, r3).Interior.Color = Excel.XlRgbColor.rgbGrey;
-                }
-                else
-                {
-                    worksheet.get_Range(r1, r3).Interior.Color = Excel.XlRgbColor.rgbWhite;
-                }
-                if (i % 5 == 0 || i == 0)
-                {
+            // Getting data
+            List<Richtperiode> rp = RichtperiodeManager.GetRichtperiodes();
+            List<Aanvraag> aanvragen = AanvraagManager.GetAanvragen();
+            List<Aanvraag> inJaar = new List<Aanvraag>();
+            //Making sure the data is in the right year
+            foreach (Aanvraag a in aanvragen) {
+                try { 
+                    if (a.Financieringsjaar == cmbFinancieringsjaar.SelectedItem.ToString())
+                    {
+                        inJaar.Add(a);
+                    }
 
-                    worksheet.get_Range(r1, r1).Value = GetMonth(m);
-                    worksheet.get_Range(r3, r3).Value = "totaal";
-                    worksheet.get_Range(r3, r3).Font.Bold = true;
-                    m++;
+                } 
+                catch {
+                    MessageBox.Show("Selecteer een financieringsjaar aub");
                 }
-                else
-                {
-                    Random rnd = new Random();
-                    int g = rnd.Next(1, 10);
-                    worksheet.get_Range(r2, r2).Value = "titel";
-                    worksheet.get_Range(r3, r3).Value = g;
-                }
-
-
             }
-            //layout
-            string richtper = cmbFinancieringsjaar.SelectedItem.ToString();
+            //setting up the numbers
+            int add = 2;
+            int ri;
+            int rs = 2;
+            //loops over the periods (m=month)
+            for (int m = 1; m <= rp.Count; m++)
+            {
+                //ri = position in int, r = position in string
+                ri = m + rs;
+                string r = (ri).ToString();
+                //the name of the month
+                worksheet.get_Range("A" + r, "A" + r).Value = RichtperiodeManager.GetRichtperiodeById(m).Naam;
+                decimal tot = 0;
+                //background color for months in Excel file
+                Color(ri, m, worksheet);
+                //Making sure the data in the right month
+                List<Aanvraag> InPer = new List<Aanvraag>();
+                foreach (Aanvraag a in inJaar)
+                {
+                    if (a.RichtperiodeId == m)
+                    {
+                        InPer.Add(a);
+                    }
+                }
+                //going over the data
+                for (int j = 0; j < InPer.Count; j++)
+                {
+                    //add = position of data, rs=offset by the data
+                    add = ri + j + 1;
+                    rs = j + 3;
+                    //calculates the price
+                    decimal prijs = InPer[j].AantalStuk + InPer[j].PrijsIndicatieStuk;
+                    //puts data on the right position (based on add)
+                    worksheet.get_Range("B" + add, "B" + add).Value = InPer[j].Titel;
+                    worksheet.get_Range("C" + add, "C" + add).Value = (prijs);
+                    //total for the month
+                    tot += prijs;
+                    //background color for data in Excel file
+                    Color(add, m, worksheet);
+                }
+                rs++;
+                Color((m+ rs), m, worksheet);
+                //puts total of month on it's spot, makes it bold
+                worksheet.get_Range("C"+r, "C"+r).Value = tot;
+                worksheet.get_Range("C"+r, "C"+r).Font.Bold = true;
+            }
+            //===============just layout=================
             //title
+            string richtper = cmbFinancieringsjaar.SelectedItem.ToString();
+
             worksheet.Cells[1, 1] = "Financieringsjaar: " + richtper;
+            //layout title
             worksheet.get_Range("A1", "A1").Font.Bold = true;
             worksheet.get_Range("A1", "A1").Font.Underline = true;
-            //B
+            //layout data
             worksheet.get_Range("B1", "B200").ColumnWidth = 55;
             worksheet.get_Range("C2", "C200").NumberFormat = "0.00 €";
-            //
+
             // Show save file dialog
             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
             //savefile dialog inputs
             saveFileDialog1.FileName = "Budgetoverzicht-" + richtper;
             saveFileDialog1.Filter = "excel files (*.xlsx)|*.xlsx";
             saveFileDialog1.FilterIndex = 1;
+            //shows save file dialog
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                worksheet.SaveAs(saveFileDialog1.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing);
-                workBook.Close(false, Type.Missing, Type.Missing);
-                app.Quit();
-                MessageBox.Show("Het Excel document staat klaar!");
+                //it doesn't like onedrive(saves the Excel file)
+                try
+                {
+                    worksheet.SaveAs(saveFileDialog1.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing);
+                    workBook.Close(false, Type.Missing, false);
+                    app.Quit();
+                    MessageBox.Show("Het Excel document staat klaar!");
+                }
+                catch
+                {
+                    MessageBox.Show("Excel doesn't like onedrive :(");
+                }
+
             }
         }
-            public string GetMonth(int i)
+        public void Color(int pos, int month, Excel.Worksheet ws)
+        {
+            if (month % 2 == 0)
             {
-            switch (i)
+                ws.get_Range("A" + pos, "C" + pos).Interior.Color = Excel.XlRgbColor.rgbGrey;
+            }
+            else
             {
-                case 1:
-                    return "januari";
-                case 2:
-                    return "februari";
-                case 3:
-                    return "maart";
-                case 4:
-                    return "april";
-                case 5:
-                    return "mei";
-                case 6:
-                    return "juni";
-                case 7:
-                    return "juli";
-                case 8:
-                    return "augustus";
-                case 9:
-                    return "september";
-                case 10:
-                    return "oktober";
-                case 11:
-                    return "november";
-                case 12:
-                    return "december";
-                default:
-                    return "";
+                ws.get_Range("A" + pos, "C" + pos).Interior.Color = Excel.XlRgbColor.rgbWhite;
             }
         }
     }
