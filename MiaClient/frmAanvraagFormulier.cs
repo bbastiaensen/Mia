63e9d44aae717726b212ae1d2b890ffa8463d4ae
William Lopes van Buchem <joaowilliam2006@hotmail.com>
2026-01-16 12:50:11 +0100

    US 258

--- MiaClient/frmAanvraagFormulier.cs
+++ MiaClient/frmAanvraagFormulier.cs
@@ -66,7 +66,7 @@ namespace MiaClient
         {
             InitializeComponent();
             vulFormulier();
-            
+
             SetFormStatus(false);
             GetParam();
            
