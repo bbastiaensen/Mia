e4001c0551eb316c0514e3ecead49891a71ff182
Ali Wollah Mohsini <aliwollah.mohsini@fluxus.nu>
2026-01-28 14:52:37 +0100

    244-Ali

--- MiaClient/frmBeheerInvesteringsType.cs
+++ MiaClient/frmBeheerInvesteringsType.cs
@@ -3,10 +3,6 @@ using MiaLogic.Object;
 using ProofOfConceptDesign;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -14,39 +10,48 @@ namespace MiaClient
 {
     public partial class frmBeheerInvesteringsType : Form
     {
-        private Investering huidige;
-        private bool isNew;
-        public event EventHandler InvesteringsTypeschanged;
+        List<Investering> investeringen;
+        public event EventHandler InvesteringsTypesChanged;
+
+        int xPos = 10;
+        int yPos = 20;
+        int grpHeight = 26;
+        bool IsNew = false;
 
         public frmBeheerInvesteringsType()
         {
             InitializeComponent();
         }
-     
+
+        private void frmBeheerInvesteringsType_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            ((Form)sender).Hide();
+
+            if (AppForms.frmBeheerInvesteringsType == this)
+            {
+                AppForms.frmBeheerInvesteringsType = null;
+            }
+        }
 
         private void frmBeheerInvesteringsType_Load(object sender, EventArgs e)
         {
             CreateUI();
-            LoadList();
-           
+            BindLstInvesteringsTypes();
+
+            AppForms.frmBeheerInvesteringsType = this;
+
+            if (AppForms.frmAanvraagFormulier != null)
+            {
+                this.InvesteringsTypesChanged -=
+                    AppForms.frmAanvraagFormulier.FrmBeheerInvesteringsType_InvesteringsTypeChanged;
+
+                this.InvesteringsTypesChanged +=
+                    AppForms.frmAanvraagFormulier.FrmBeheerInvesteringsType_InvesteringsTypeChanged;
+            }
         }
 
-        //    btnNieuw.Click += btnNieuw_Click;
-        //    btnBewaren.Click += btnBewaren_Click;
-        //    btnVerwijderen.Click += btnVerwijderen_Click;
-        //    InvesteringsTypes.SelectedIndexChanged += InvesteringsTypes_SelectedIndexChanged;
-
-        //    AppForms.frmBeheerInvesteringsType = this;
-
-        //    if (AppForms.frmAanvraagFormulier != null)
-        //    {
-        //        this.InvesteringsTypeschanged -= AppForms.frmAanvraagFormulier.FrmBeheerInvesteringsType_InvesteringsTypeChanged;
-        //        this.InvesteringsTypeschanged += AppForms.frmAanvraagFormulier.FrmBeheerInvesteringsType_InvesteringsTypeChanged;
-        //    }
-        //}
-
-
-        private void CreateUI()
+        public void CreateUI()
         {
             this.BackColor = StyleParameters.Achtergrondkleur;
 
@@ -59,36 +64,45 @@ namespace MiaClient
             }
         }
 
-        private void LoadList()
+        public void BindLstInvesteringsTypes()
         {
-            InvesteringsTypes.DataSource = null;
-            InvesteringsTypes.DataSource = InvesteringenManager.GetInvesteringen();
+            investeringen = InvesteringenManager.GetInvesteringen();
+
+            InvesteringsTypes.DisplayMember = "Naam";
+            InvesteringsTypes.ValueMember = "Id";
+            InvesteringsTypes.DataSource = investeringen;
         }
 
         private void InvesteringsTypes_SelectedIndexChanged(object sender, EventArgs e)
         {
-            huidige = InvesteringsTypes.SelectedItem as Investering;
-            if (huidige == null) return;
+            Investering investering = (Investering)InvesteringsTypes.SelectedItem;
 
-            txtId.Text = huidige.Id.ToString();
-            txtVoornaam.Text = huidige.Naam;
-            checkActief.Checked = huidige.Actief;
-            isNew = false;
+            if (investering != null)
+            {
+                txtId.Text = investering.Id.ToString();
+                txtNaam.Text = investering.Naam;
+                checkActief.Checked = investering.Actief;
+
+                IsNew = false;
+            }
+        }
+
+        private void ClearFields()
+        {
+            txtId.Text = string.Empty;
+            txtNaam.Text = string.Empty;
+            checkActief.Checked = false;
+            IsNew = true;
         }
 
         private void btnNieuw_Click(object sender, EventArgs e)
         {
-            huidige = new Investering();
-            isNew = true;
-
-            txtId.Clear();
-            txtVoornaam.Clear();
-            checkActief.Checked = true;
+            ClearFields();
         }
 
         private void btnBewaren_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(txtVoornaam.Text))
+            if (string.IsNullOrWhiteSpace(txtNaam.Text))
             {
                 MessageBox.Show("Naam is verplicht");
                 return;
@@ -96,66 +110,54 @@ namespace MiaClient
 
             Investering i = new Investering();
 
-            if (!isNew && InvesteringsTypes.SelectedItem is Investering selected)
+            if (!IsNew && InvesteringsTypes.SelectedValue != null)
             {
-                i.Id = selected.Id;
+                i.Id = Convert.ToInt32(InvesteringsTypes.SelectedValue);
             }
 
-            i.Naam = txtVoornaam.Text;
+            i.Naam = txtNaam.Text;
             i.Actief = checkActief.Checked;
 
-            i.Id = InvesteringenManager.SaveInvestering(i, isNew);
+            i.Id = InvesteringenManager.SaveInvestering(i, IsNew);
+            InvesteringsTypesChanged?.Invoke(this, EventArgs.Empty);
 
-            LoadList();
+            BindLstInvesteringsTypes();
+            ClearFields();
+            InvesteringsTypes.SelectedValue = i.Id;
+            IsNew = false;
 
-            InvesteringsTypeschanged?.Invoke(this, EventArgs.Empty);
-
-            InvesteringsTypes.SelectedItem = i; 
-            isNew = false;
-
-            MessageBox.Show("De gegevens werden succesvol bewaard.", "MIA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(
+                "De gegevens werden succesvol bewaard.",
+                "MIA",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
         }
 
 
         private void btnVerwijderen_Click(object sender, EventArgs e)
         {
-            if (huidige == null) return;
-
-            if (MessageBox.Show(
-                "Ben je zeker dat je dit InvesteringsType wil verwijderen?",
-                "Bevestiging",
-                MessageBoxButtons.YesNo,
-                MessageBoxIcon.Warning) == DialogResult.Yes)
+            Investering i = new Investering();
+            i.Id = Convert.ToInt32(InvesteringsTypes.SelectedValue);
+            i.Naam = txtNaam.Text;
+            if (checkActief.Checked)
             {
-                InvesteringenManager.DeleteInvestering(huidige);
-                LoadList();
-                btnNieuw_Click(null, null);
+                i.Actief = true;
             }
-            InvesteringsTypeschanged?.Invoke(this, EventArgs.Empty);
-        }
-
-        private void frmBeheerInvesteringsType_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            e.Cancel = true;
-            this.Hide();
-            if (AppForms.frmBeheerInvesteringsType == this)
+            else
             {
-                AppForms.frmBeheerInvesteringsType = null;
+                i.Actief = false;
+
             }
+
+            if (MessageBox.Show($"Bent u dat u {InvesteringsTypes.Text} wilt verwijderen?", "InvesteringsType verwijderen", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                MessageBox.Show("De InvesteringsType is succesvol verwijderd", "MIA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                InvesteringenManager.DeleteInvestering(i);
+                InvesteringsTypesChanged?.Invoke(this, EventArgs.Empty);
+            }
+
+            BindLstInvesteringsTypes();
+            ClearFields();
         }
-
-       
-        
-
-   
-       
-      
-
-      
-
-
-
-  
     }
 }
-
