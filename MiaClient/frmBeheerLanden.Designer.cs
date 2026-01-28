dba4c0b87d538ac8bd13727e5aff6edb02a8fabf
Rune Van Hofstraeten <rune.van.hofstraeten@365.academicoffice.be>
2026-01-28 15:45:55 +0100

    bezig met het beheer van landen

---
+++ MiaClient/frmBeheerLanden.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MiaClient
+{
+    partial class frmBeheerLanden
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBewaren = new System.Windows.Forms.Button();
+            this.btnVerwijderen = new System.Windows.Forms.Button();
+            this.btnNieuw = new System.Windows.Forms.Button();
+            this.txtNaam = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblNaam = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.LstLanden = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnBewaren
+            // 
+            this.btnBewaren.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBewaren.Location = new System.Drawing.Point(425, 103);
+            this.btnBewaren.Name = "btnBewaren";
+            this.btnBewaren.Size = new System.Drawing.Size(105, 33);
+            this.btnBewaren.TabIndex = 23;
+            this.btnBewaren.Text = "Bewaren";
+            this.btnBewaren.UseVisualStyleBackColor = true;
+            this.btnBewaren.Click += new System.EventHandler(this.btnBewaren_Click);
+            // 
+            // btnVerwijderen
+            // 
+            this.btnVerwijderen.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerwijderen.Location = new System.Drawing.Point(536, 103);
+            this.btnVerwijderen.Name = "btnVerwijderen";
+            this.btnVerwijderen.Size = new System.Drawing.Size(105, 33);
+            this.btnVerwijderen.TabIndex = 22;
+            this.btnVerwijderen.Text = "Verwijderen";
+            this.btnVerwijderen.UseVisualStyleBackColor = true;
+            this.btnVerwijderen.Click += new System.EventHandler(this.btnVerwijderen_Click);
+            // 
+            // btnNieuw
+            // 
+            this.btnNieuw.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNieuw.Location = new System.Drawing.Point(314, 103);
+            this.btnNieuw.Name = "btnNieuw";
+            this.btnNieuw.Size = new System.Drawing.Size(105, 33);
+            this.btnNieuw.TabIndex = 21;
+            this.btnNieuw.Text = "Nieuw";
+            this.btnNieuw.UseVisualStyleBackColor = true;
+            this.btnNieuw.Click += new System.EventHandler(this.btnNieuw_Click);
+            // 
+            // txtNaam
+            // 
+            this.txtNaam.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNaam.Location = new System.Drawing.Point(428, 65);
+            this.txtNaam.Name = "txtNaam";
+            this.txtNaam.Size = new System.Drawing.Size(213, 27);
+            this.txtNaam.TabIndex = 19;
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(428, 26);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(213, 27);
+            this.txtId.TabIndex = 18;
+            // 
+            // lblNaam
+            // 
+            this.lblNaam.AutoSize = true;
+            this.lblNaam.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNaam.Location = new System.Drawing.Point(326, 65);
+            this.lblNaam.Name = "lblNaam";
+            this.lblNaam.Size = new System.Drawing.Size(56, 20);
+            this.lblNaam.TabIndex = 15;
+            this.lblNaam.Text = "Naam: ";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(326, 29);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(29, 20);
+            this.lblId.TabIndex = 13;
+            this.lblId.Text = "Id: ";
+            // 
+            // LstLanden
+            // 
+            this.LstLanden.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LstLanden.FormattingEnabled = true;
+            this.LstLanden.ItemHeight = 20;
+            this.LstLanden.Location = new System.Drawing.Point(12, 12);
+            this.LstLanden.Name = "LstLanden";
+            this.LstLanden.Size = new System.Drawing.Size(289, 124);
+            this.LstLanden.TabIndex = 12;
+            this.LstLanden.SelectedIndexChanged += new System.EventHandler(this.LstLanden_SelectedIndexChanged);
+            // 
+            // frmBeheerLanden
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 154);
+            this.Controls.Add(this.btnBewaren);
+            this.Controls.Add(this.btnVerwijderen);
+            this.Controls.Add(this.btnNieuw);
+            this.Controls.Add(this.txtNaam);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblNaam);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.LstLanden);
+            this.Name = "frmBeheerLanden";
+            this.Text = "frmBeheerLanden";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBeheerLanden_FormClosing);
+            this.Load += new System.EventHandler(this.frmBeheerLanden_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBewaren;
+        private System.Windows.Forms.Button btnVerwijderen;
+        private System.Windows.Forms.Button btnNieuw;
+        private System.Windows.Forms.TextBox txtNaam;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblNaam;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.ListBox LstLanden;
+    }
+}
---
+++ MiaClient/frmBeheerLanden.cs
@@ -0,0 +1,144 @@
+﻿using ProofOfConceptDesign;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiaLogic.Manager;
+using MiaLogic.Object;
+
+namespace MiaClient
+{
+    public partial class frmBeheerLanden : Form
+    {
+        List<Land> landen;
+
+
+        int xPos = 10;
+        int yPos = 20;
+        int grpHeight = 26;
+        bool IsNew = false;
+
+        public frmBeheerLanden()
+        {
+            InitializeComponent();
+        }
+        public void CreateUI()
+        {
+            this.BackColor = StyleParameters.Achtergrondkleur;
+
+            //this.Icon = imgFormIcon;
+
+            foreach (var btn in this.Controls.OfType<Button>())
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 0;
+                btn.BackColor = StyleParameters.ButtonBack;
+                btn.ForeColor = StyleParameters.Buttontext;
+            }
+        }
+
+        private void frmBeheerLanden_Load(object sender, EventArgs e)
+        {
+            CreateUI();
+            BindLstLanden();
+        }
+
+        private void frmBeheerLanden_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //We sluiten het formulier niet, maar verbergen het. Zo voorkomen we dat het formulier meerdere
+            //keren naast elkaar kan geopend worden.
+            e.Cancel = true;
+            ((Form)sender).Hide();
+        }
+        public void BindLstLanden()
+        {
+
+            landen = LandenManager.GetLanden();
+            LstLanden.DisplayMember = "Naam";
+
+            LstLanden.ValueMember = "Id";
+            LstLanden.DataSource = landen;
+
+        }
+
+        private void LstLanden_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Land land = (Land)LstLanden.SelectedItem;
+
+
+            if (land != null)
+            {
+                txtId.Text = Convert.ToString(land.Id);
+                txtNaam.Text = land.Naam;
+              
+
+                IsNew = false;
+            }
+        }
+        private void ClearFields()
+        {
+            txtNaam.Text = string.Empty;
+            txtId.Text = string.Empty;
+           
+            IsNew = true;
+        }
+
+        private void btnNieuw_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void btnBewaren_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace( txtNaam.Text))
+            {
+                MessageBox.Show("Gelieve een geldige naam in te vullen.", "MIA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            txtNaam.Text = txtNaam.Text.Trim();
+            Land l = new Land();
+            l.Id = Convert.ToInt32(LstLanden.SelectedValue);
+            l.Naam = txtNaam.Text;
+            l.Id = LandenManager.SaveLanden(l, IsNew);
+            BindLstLanden();
+           
+            LstLanden.SelectedValue = l.Id;
+            IsNew = false;
+
+            MessageBox.Show("De gegevens werden succesvol bewaard.", "MIA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnVerwijderen_Click(object sender, EventArgs e)
+        {
+            Land l = new Land();
+            l.Id = Convert.ToInt32(LstLanden.SelectedValue);
+            l.Naam = txtNaam.Text;
+           
+
+            if (MessageBox.Show($"Bent u zeker dat u {LstLanden.Text} wilt verwijderen?", "Land verwijderen", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try 
+                {
+                    
+                    LandenManager.DeleteLand(l);
+                }
+                  catch (Exception ex)
+                  {
+                      MessageBox.Show($"Fout bij verwijderen van het land: {ex.Message}", "MIA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                      return;
+                }
+                LandenManager.DeleteLand(l);
+                MessageBox.Show("Het Land is succesvol verwijderd", "MIA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            BindLstLanden();
+            ClearFields();
+        }
+    }
+}
